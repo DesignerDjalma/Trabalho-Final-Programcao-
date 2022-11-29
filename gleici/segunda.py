c080e000894c0cf0c4d0a49125829a2c76eb56d8
Djalma Filho <designer.djalma@gmail.com>
2022-11-29 11:58:15 -0300

    Final Ups

--- gleici/segunda.py
+++ gleici/segunda.py
@@ -1,28 +1,36 @@
 NossaLista = [1,2,3,4,1,5,7,1,9,10]
 print("Forneça a lista com 10 items. Ex.: [1,2,3,4,1,5,7,1,9,10]")
+
 NossaLista = input("lista: ")
 NossaLista2 = []
+
 for i in NossaLista.replace("[","").replace("]","").split(','):
     NossaLista2.append(float(i))
+
 def retornoDeMaiorElemento(lista):
     print("Maior elemento:", max(lista))
     pass
+
 def retorneSomaDeElementos(lista):
     print("Soma dos elementos:", sum(lista))
     pass
+
 def retorneNumeroOcorrenciaPrimeiroElementoDaLista(lista):
     PrimeiroElemento = lista[0]
     NumeroDeRepeticao = lista.count(PrimeiroElemento)
     print("Ocorrencia do primeiro elemento:", NumeroDeRepeticao)
     pass
+
 def retorneMediaElementos(lista):
     Comprimento=len(lista)
     Soma=sum(lista)
     Media=Soma/Comprimento
     print("Media dos elementos:", Media)
     pass
+
 def retorneValorMaisProximoDaMediaDosElementos(lista):
     pass
+
 def retorneSomaDosElementoComValorNegativo(lista):
     SomaDosElementos=0
     for item in lista:
--- gleici/terceira.py
+++ gleici/terceira.py
@@ -1,15 +1,18 @@
+
 def criarCaminhao(novo=''):
     return Caminhao(
         input(f"{novo}Nome Caminhão: "),
         int(input(f"{novo}número do ID: ")),
         float(input(f"{novo}Carga Máxima: ")),
         float(input(f"{novo}Custo por Km: ")))
+
 def criarCarga(novo=''):
     return Carga(
         float(input(f"{novo}Peso: ")),
         float(input(f"{novo}Distancia: ")),
         input(f"{novo}Local de Origem: "),
         input(f"{novo}Local de Destino: "))
+
 def listaCarga():
     print("Lista de Cargas: ")
     for i in range(len(cargas)):
@@ -18,6 +21,7 @@ def listaCarga():
         "Distancia:", cargas[i].distancia,
         "Local Origem:", cargas[i].origem,
         "Destino:", cargas[i].destino,)
+
 def listaCaminhao():
     print("Lista de Caminhões: ")
     for i in range(len(caminhoes)):
@@ -28,37 +32,50 @@ def listaCaminhao():
         "Custo por Km:",caminhoes[i].custo,
         "Cargas: ",end='')
         listaCargaCaminhao(caminhoes[i].cargas)
+
 def listaCargaCaminhao(caminhao):
+    
     if caminhao:
         for i in range(len(caminhao)):
             print(f"carga [{i+1}] ",end='')
     print()
+
 def listaCustoCaminhao(caminhao):
     custo_cargas = []
     for i in caminhao.cargas:
         valor = caminhao.custo * i.distancia/1000
         custo_cargas.append(valor)
     return sum(custo_cargas)
+
 def listaCustoTotal():
     custo_caminhoes = []
     for i in caminhoes:
         custo_caminhoes.append(listaCustoCaminhao(i))
     return sum(custo_caminhoes)
+
 cargas = []
 caminhoes = []
+
+
 class Carga:
+    
     def __init__(self, peso, distancia, origem, destino) -> None:
         self.peso = peso
         self.distancia = distancia
         self.origem = origem
         self.destino = destino   
+
+
 class Caminhao:
+    
     def __init__(self, nome, id2, carga_max, custo) -> None:
         self.nome = nome
         self.id2 = id2
         self.carga_max = carga_max
         self.custo = custo
         self.cargas = []
+
+
 while 1:
     print("\nMenu\n")
     print("1 -> Criar Caminhão")
@@ -74,32 +91,42 @@ while 1:
     print("11-> Enter para Sair")
     funcao=int(input("\nEscolha: "))
     print('\n\t',end='')
+    
     if funcao==11:
         break
+    
     if funcao==1:
         caminhoes.append(criarCaminhao())
+    
     if funcao==2:
         listaCaminhao()
+    
     if funcao==3:
         print("Deletar Qual Caminhões: ")
         listaCaminhao()
         del caminhoes[int(input("Caminhão número: "))-1]
+    
     if funcao==4:
         print("Modificar Qual Caminhões: ")
         listaCaminhao()
         caminhoes[int(input("Caminhão número: "))-1] = criarCaminhao("Novo Valor para ")
+    
     if funcao==5:
         cargas.append(criarCarga())
+    
     if funcao==6:
         listaCarga()
+    
     if funcao==7:
         print("Deletar Qual Carga: ")
         listaCarga()
         del cargas[int(input("Carga número: "))-1]
+    
     if funcao==8:
         print("Modificar Qual Carga: ")
         listaCarga()
         caminhoes[int(input("Caminhão número: "))-1] = criarCarga("Novo Valor para ")
+    
     if funcao==9:
         print("Adicionar Qual Carga em Qual Caminhão: ")
         listaCaminhao()
@@ -109,10 +136,12 @@ while 1:
         pesosTotais = 0
         for i in caminhoes[cami-1].cargas:
             pesosTotais += i.peso
+        
         if cargas[carg-1].peso <= caminhoes[cami-1].carga_max - pesosTotais:
             caminhoes[cami-1].cargas.append(cargas[carg-1])
         else:
             print("Carga muito pesado pro caminhão!")
+    
     if funcao==10:
         print("Lista o Custo dos Caminhões:")
         contador = 0
@@ -120,5 +149,7 @@ while 1:
             contador += 1
             print(f"Custo Caminhão {contador} ->", listaCustoCaminhao(i))
         print(f"Custo Total: {listaCustoTotal()}")
+
+
 print("saindo")
 
